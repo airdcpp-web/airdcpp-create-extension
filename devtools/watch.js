8871a175028acf5df469620552c5a5da954457f1
maksis <maksis@adrenaline-network.com>
2017-03-19 14:51:51 +0200

    Dev server: improve error reporting

--- devtools/watch.js
+++ devtools/watch.js
@@ -9,9 +9,21 @@ const webpackConfig = require('../webpack.config.js');
 let started = false;
 
 webpack(webpackConfig).watch(250, function(err, stats) {
-	if (err || stats.hasErrors()) {
-		console.error(err);
-		return;
+	if (err) {
+    console.error(err.stack || err);
+    if (err.details) {
+      console.error(err.details);
+    }
+    return;
+	}
+
+	const info = stats.toJson();
+	if (stats.hasErrors()) {
+		console.error(info.errors);
+	}
+
+	if (stats.hasWarnings()) {
+		console.warn(info.warnings)
 	}
 
 	if (!started) {
